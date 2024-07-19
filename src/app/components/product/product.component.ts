8106ae3d7389cb946825be412fdf95346ac8b188
Evil Genius <immanuelnganga2001@gmail.com>
2024-07-19 03:41:30 +0300

    pipes in angular

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -6,12 +6,14 @@ import { ButtonModule } from 'primeng/button';
 import { ConfirmationService } from 'primeng/api';
 import { ConfirmPopupModule } from 'primeng/confirmpopup';
 import { ToastModule } from 'primeng/toast';
+import { PricePipe } from '../../pipes/price.pipe';
+import { TruncateNamePipe } from '../../pipes/truncate-name.pipe';
 
 @Component({
   selector: 'app-product',
   standalone: true,
   imports: [
-    RatingModule, FormsModule, ButtonModule,  ConfirmPopupModule, ToastModule
+    RatingModule, FormsModule, ButtonModule,  ConfirmPopupModule, ToastModule, PricePipe, TruncateNamePipe
   ],
   providers: [ConfirmationService],
   templateUrl: './product.component.html',
